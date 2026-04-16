1d71bb288a06bba2beccb7b01f77f4184b25a4b2
Thiyumi2003 <thiyumiupasari2003@gmail.com>
2026-04-17 03:45:57 +0530

    add frontend url

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,16 @@ const dashboardRoutes = require("./routes/dashboard.routes.js");
 
 const app = express();
 
+
+
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://lemon-sand-00cd61800.6.azurestaticapps.net"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
