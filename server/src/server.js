fe152171713f01704b7c13da01de6c8ece25686d
Thiyumi2003 <thiyumiupasari2003@gmail.com>
2026-04-16 23:24:44 +0530

    updated

--- server/src/server.js
+++ server/src/server.js
@@ -3,19 +3,12 @@ const app = require("./app.js");
 
 const PORT = process.env.PORT || 5000;
 
-console.log("SERVER STARTUP: Loading init...");
+// Import init for DB bootstrap side effects.
+require("./db/init.js");
 
-// Import and call init
-const init = require("./db/init.js");
-
-console.log("SERVER STARTUP: Init loaded, setting up listener...");
-
-// The init file calls initDatabase() directly, so we just need to start the server
-const server = app.listen(PORT, () => {
-  console.log(`✓ Server listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-console.log("SERVER STARTUP: Listener created, server should be running...");
 
 // Keep the process alive
 process.on("uncaughtException", (err) => {
